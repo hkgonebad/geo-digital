4a9c262b33716985aa34d242b356a5d9273ab16f
hkgonebad <harry007007@gmail.com>
2025-05-17 00:07:02 +0530

    Update Vite configuration to set dynamic base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/geo-digital/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -14,5 +15,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/geo-digital/",
 }));
